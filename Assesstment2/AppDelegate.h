a967d73249ea727e237ea48584e83ef49ac3221d
Efrén Reyes <efren.reyes@globant.com>
2014-07-31 13:56:58 -0500

    Initial Commit

---
+++ Assesstment2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Assesstment2
+//
+//  Created by Efrén Reyes Torres on 7/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
